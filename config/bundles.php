3f94dde4299e35db5238d92e3b13d661dc74df06
Dan Abrey <danabrey.com>
2019-10-05 17:48:24 +0100

    remove web server bundle and update deps

--- config/bundles.php
+++ config/bundles.php
@@ -5,5 +5,4 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
-    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
 ];
